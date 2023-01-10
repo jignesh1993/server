0ba965806bb8248ea1aa70977fe05543275b21fa
jignesh1993 <jignesh.mahesh07@gmail.com>
2023-01-10 22:02:55 +0530

    work on like and delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,10 +12,9 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   const post = req.body;
-  console.log('post -->>', post);
   const newPost = new PostMessage(post);
   try {
-    await newPost.save(); 
+    await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
@@ -26,7 +25,36 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send("No post with that id");
+    await PostMessage.findByIdAndDelete(_id);
+    res.status(200).json({ message: "Post deleted successfully!" });
+  } catch (error) {
+    console.log("error -->>", error);
+  }
+};
+
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params; 
+  try { 
+    const post = await PostMessage.findById(_id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {likeCount: post.likeCount + 1}, {
+      new: true,
+    });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    console.log("error -->>", error);
+  }
 };
